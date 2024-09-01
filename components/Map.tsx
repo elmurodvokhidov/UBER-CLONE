ae50d74c6f95796eef3914872d8de6325827083f
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-01 17:47:14 +0500

    map functionality

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import MapView, { PROVIDER_DEFAULT } from 'react-native-maps'
 
 const Map = () => {
+    // const region = {}
+
     return (
-        <View>
-            <Text>Map</Text>
-        </View>
+        <MapView
+            provider={PROVIDER_DEFAULT}
+            className='w-full h-full rounded-2xl'
+            tintColor='black'
+            mapType='mutedStandard'
+            showsPointsOfInterest={false}
+            // initialRegion={region}
+            showsUserLocation={true}
+            userInterfaceStyle='light'
+        >
+        </MapView>
     )
 }
 
